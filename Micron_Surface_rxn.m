26b702439087ccd5390305b9d5e610137abeb7b6
RosbyR <roro2148@colorado.edu>
2025-02-24 15:57:08 -0700

    Surface rxn 3.0

--- Micron_Surface_rxn.m
+++ Micron_Surface_rxn.m
@@ -1,31 +1,44 @@
-% Retrieve constants
-[R, T, A_SiH4, E_SiH4, A_SiH2, E_SiH2, A_H2des, E_H2des] = Micron_constants1();
+[R, T, k1, SiH4_conc, tspan, P, delta, D_SiH4, D_SiH2, D_H2, ...
+ A_SiH4, E_SiH4, A_SiH2, E_SiH2, A_H2des, E_H2des] = Micron_constants1();
 
 
 [SiH4_b, SiH2_b, H2_b] = Micron_gasphase();
-
-%Compute rate based on first order kinetics
-k_silane   = A_SiH4   * exp(-E_SiH4   / (R*T));
-k_disilane = A_SiH2 * exp(-E_SiH2 / (R*T));
-k_H2des    = A_H2des    * exp(-E_H2des    / (R*T));
-
+% Ensure that the outputs are scalars:
+SiH4_b = SiH4_b(1);
+SiH2_b = SiH2_b(1);
+H2_b   = H2_b(1);
 
 
-y0 = [SiH4_b; SiH2_b; H2_b; 0;]
 
-function dydt = SurfaceRxnODE(~, y, R, T, k_silane, k_disilane, k_H2des)
-    % Unpack the variables (example: partial pressures or concentrations)
-    p_SiH4  = y(1);
-    p_Si2H6 = y(2);
-    p_H2    = y(3);
-    theta_H = y(4);  % surface coverage, for instance
+% Compute rate constants based on first order kinetics
+k_silane   = A_SiH4  * exp(-E_SiH4  / (R*T));
+k_disilane = A_SiH2  * exp(-E_SiH2  / (R*T));
+k_H2des    = A_H2des * exp(-E_H2des / (R*T));
 
-% Example ODEs (placeholders):
-    dp_SiH4_dt  = -k_silane * p_SiH4;  
-    dp_Si2H6_dt = k_silane * p_SiH4 - k_disilane * p_Si2H6;
-    dp_H2_dt    = 2 * k_silane * p_SiH4 - k_H2des * p_H2;
-    dtheta_H_dt = 0; % update based on adsorption/desorption dynamics
+% Initial conditions (column vector)
+y0 = [SiH4_b; SiH2_b; H2_b];
+
+% Use the corrected parameter order
+[t, y] = ode15s(@(t, y) SurfaceRxnODE(t, y, R, k_silane, k_disilane, k_H2des), tspan, y0);
+
+% Display final concentrations
+disp('Final surface concentrations:');
+disp(['SiH4_s: ', num2str(y(end, 1))]);
+disp(['SiH2_s: ', num2str(y(end, 2))]);
+disp(['H2_s:   ', num2str(y(end, 3))]);
+
+% ODE function definition with debugging
+function dydt = SurfaceRxnODE(~, y, R, k_silane, k_disilane, k_H2des)
+    % Unpack current concentrations
+    p_SiH4 = y(1);
+    p_SiH2 = y(2);
+    p_H2   = y(3);
     
-    dydt = [dp_SiH4_dt; dp_Si2H6_dt; dp_H2_dt; dtheta_H_dt];
+    % Compute derivatives (using scalar multiplication)
+    dp_SiH4_dt = -k_silane * p_SiH4;  
+    dp_SiH2_dt = -k_disilane * p_SiH2;
+    dp_H2_dt   = -k_H2des  * p_H2;
+    
+    % Concatenate into a column vector
+    dydt = [dp_SiH4_dt; dp_SiH2_dt; dp_H2_dt];
 end
-
--- Micron_constants1.m
+++ Micron_constants1.m
@@ -1,35 +1,34 @@
-function [R, T, k1, SiH4_conc, tspan, P, delta, D_SiH4, D_SiH2, ...
-    D_H2,A_SiH4, E_SiH4, A_SiH2, E_SiH2, A_H2des, E_H2des] = Micron_constants1()
-    % Universal Gas Constant
+function[R, T, k1, SiH4_conc, tspan, P, delta, D_SiH4, D_SiH2, D_H2, ...
+ A_SiH4, E_SiH4, A_SiH2, E_SiH2, A_H2des, E_H2des] = Micron_constants1();
+
+    
+% Universal Gas Constant
     R = 8.314;       % J/(mol*K)
     
     % System Conditions
     T = 273 + 600;        % Temperature (K)
     P = 66.6612;      % Pressure (Pa)
     delta = 1e-4;    % Boundary layer thickness (m)  
-    %**No Idea if this is a reasonable boundary layer
     
-    % Reaction Rate Constants
-    k1 = 1e5;        % Rate constant for SiH4 decomposition (1/s)
-
-    % Initial Concentrations
-    SiH4_conc = 1;   % Initial concentration of SiH4 (mol/m^3)
+    % Reaction Rate Constants (unused in your main code)
+    k1 = 1e5;        
+    
+    % Initial Concentrations (unused in your main code)
+    SiH4_conc = 1;   
     
     % Time Span
-    tspan = [0 1e-4]; % Time span for reaction (seconds)
+    tspan = [0, 1e-4]; % Time span for reaction (seconds)
 
-    % **Dummy Diffusivities** (m²/s) - Example values for gases
-    D_SiH4 = 1e-5;  % Diffusivity of SiH4 (assumed)
-    D_SiH2   = 5e-6;  % Diffusivity of SiH2 (assumed)
-    D_H2   = 7e-5;  % Diffusivity of H2 (assumed)
+    % Diffusivities (unused in your main code)
+    D_SiH4 = 1e-5;
+    D_SiH2 = 5e-6;
+    D_H2   = 7e-5;
 
-
-    %Surface adsorption constants
+    % Surface adsorption constants
     A_SiH4 = 8.39e26;
     E_SiH4 = 37450.0;
     A_SiH2 = 8.39e27;
     E_SiH2 = 37450.0;
     A_H2des = 1.75e20;
     E_H2des = 47000.0;
-
 end
